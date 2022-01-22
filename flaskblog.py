7d8a865d5449a938849f3840f1305b44ae9d0741
dawidwojdylak <dawidwojdylak98@gmail.com>
2022-01-22 19:11:26 +0100

    tut3

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, flash, redirect, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = 'a68cabeddf4b2d08ce3a10d2e758fb382f70d3fccb3b385b15b0495847354c0e'
 
 data = [
     {
@@ -25,6 +28,21 @@ def hello_world():
 @app.route("/about")
 def about():
     return render_template('about.html', title="About")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title="Register", form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title="Login", form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
