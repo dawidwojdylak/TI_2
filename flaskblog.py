5317100fdce3708fd909af8dd0a3ab2d7d11c767
dawidwojdylak <dawidwojdylak98@gmail.com>
2022-01-22 18:34:37 +0100

    tut2

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+data = [
+    {
+        'author' : 'Jane Doe',
+        'title' : 'Blog post 1',
+        'content' : 'first post content',
+        'date' : 'Jan 22'
+    },
+        {
+        'author' : 'Jane Doe',
+        'title' : 'Blog post 2',
+        'content' : 'second post content',
+        'date' : 'Jan 22'
+    }
+]
+
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World123!</p>"
-
+    return render_template('home.html', posts=data)
 
 @app.route("/about")
 def about():
-    return "<p>About</p>"
+    return render_template('about.html', title="About")
 
 if __name__ == '__main__':
     app.run(debug=True)
